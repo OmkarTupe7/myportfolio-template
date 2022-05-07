70628f33154382613dd6f2f8cd5b295406a11878
zougari47 <zougariahmed007@gmail.com>
2022-05-07 13:17:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Description
+
+My portfolio website ✍ ...
---
+++ src/js/typed.js
@@ -0,0 +1,10 @@
+import Typed from 'typed.js';
+
+var options = {
+  strings: ["I'm a Front-end Developer", 'I Build  Websites'],
+  typeSpeed: 150,
+  backSpeed: 50,
+  backDelay: 2000
+};
+
+var typed = new Typed('#headline', options);
